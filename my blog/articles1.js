6485e93cc6dea95972e04e77a51ade5c22b86f2e
好酒 <penggan0@hotmail.com>
2024-07-01 08:48:01 +0800

    2024.7.1

---
+++ my blog/articles1.js
@@ -0,0 +1,43 @@
+// articles1.js
+const articlesPart1 = [
+    { title: "甲骨文云注册", link: "articles/articles/article1.html", lastModified: "2024-06-07 21:03:00" },
+    { title: "shadowsocks", link: "articles/articles/article2.html", lastModified: "2024-06-07 22:30:00" },
+    { title: "V2Ray一键安装脚本", link: "articles/articles/article3.html", lastModified: "2024-06-06 10:17:00" },
+    { title: "乌客玩客云小雅刷机游戏", link: "articles/articles/article4.html", lastModified: "2024-06-08 14:45:00" },
+    { title: "玩客云刷CasaOS教程", link: "articles/articles/article5.html", lastModified: "2024-06-09 08:30:00" },
+    { title: "TV更改NTP&刷机", link: "articles/articles/article6.html", lastModified: "2024-06-09 16:20:00" },
+    { title: "高效的仿站神器", link: "articles/articles/article7.html", lastModified: "2024-06-09 17:00:00" },
+    { title: "工作自信小技巧", link: "articles/articles/article8.html", lastModified: "2024-06-10 13:10:00" },
+    { title: "7个非常有意思的暗网", link: "articles/articles/article9.html", lastModified: "2024-06-11 11:55:00" },
+    { title: "咪咕盒子 MIGU-JT-U2", link: "articles/articles/article10.html", lastModified: "2024-06-11 10:05:00" },
+    { title: "开发者office", link: "articles/articles/article11.html", lastModified: "2024-06-11 14:40:00" },
+    { title: "iStoreOS教程", link: "articles/articles/article12.html", lastModified: "2024-06-11 09:00:00" },
+    { title: "Windows11精简版", link: "articles/articles/article13.html", lastModified: "2024-06-11 09:01:00" },
+    { title: "辣娃娃战神s905l3a游戏", link: "articles/articles/article14.html", lastModified: "2024-06-12 09:01:00" },
+    { title: "TV盒子固件", link: "articles/articles/article15.html", lastModified: "2024-06-14 09:01:00" },
+    { title: "Office 2010", link: "articles/articles/article16.html", lastModified: "2024-06-14 09:11:00" }
+    { title: "HirensBoot PE工具", link: "articles/articles/article17.html", lastModified: "2024-06-16 09:01:00" },
+    { title: "门禁", link: "articles/articles/article18.html", lastModified: "2024-06-16 09:11:00" },
+    { title: "linux命令", link: "articles/articles/article19.html", lastModified: "2024-06-17 09:11:00" },
+    { title: "电脑开机密码", link: "articles/articles/article20.html", lastModified: "2024-06-18 09:11:00" },
+    { title: "905游戏盒子 工具", link: "articles/articles/article21.html", lastModified: "2024-06-18 10:11:00" },
+    { title: "905游戏盒子 dtb", link: "articles/articles/article22.html", lastModified: "2024-06-18 11:11:00" },
+    { title: "开心电视助手6.2", link: "articles/articles/article23.html", lastModified: "2024-06-19 11:11:00" },
+    { title: "最好用的视频播放软件", link: "articles/articles/article24.html", lastModified: "2024-06-20 11:11:00" },
+    { title: "clouflare开源图床", link: "articles/articles/article25.html", lastModified: "2024-06-20 12:11:00" },
+    { title: "安卓最好的音乐播放器", link: "articles/articles/article26.html", lastModified: "2024-06-21 12:11:00" },
+    { title: "Emuelec菜单汉化s905", link: "articles/articles/article27.html", lastModified: "2024-06-22 12:11:00" },
+    { title: "Emuelec4.6 s905x2x3X4-s922", link: "articles/articles/article28.html", lastModified: "2024-06-23 12:11:00" },
+    { title: "s905小容量TF卡刷入大镜像", link: "articles/articles/article29.html", lastModified: "2024-06-23 16:21:00" },
+    { title: "网址大全", link: "articles/articles/article30.html", lastModified: "2094-06-23 16:21:00" },
+    { title: "盘搜", link: "articles/articles/article31.html", lastModified: "2094-06-25 16:21:00" },
+    { title: "10年免费主机部署vless节点", link: "articles/articles/article32.html", lastModified: "2024-06-25 16:21:00" },
+    { title: "ClouDNS托管到cloudflare", link: "articles/articles/article33.html", lastModified: "2024-06-25 19:21:00" },
+    { title: "提取手机中的APK文件", link: "articles/articles/article34.html", lastModified: "2024-06-29 00:21:00" },
+    { title: "Telegram接入ChatGPT-4", link: "articles/articles/article35.html", lastModified: "2024-06-29 00:31:00" },
+
+
+
+
+
+];
---
+++ my blog/articles2.js
@@ -0,0 +1,31 @@
+// articles2.js
+const articlesPart2 = [
+    { title: "linux命令大全", link: "articles/articles/article36.html", lastModified: "2024-06-29 00:41:00" }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+];
